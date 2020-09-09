470818d05cc86696ab0243f7a8f8d28e83b910c9
Shelley <sascottcollins@gmail.com>
2020-09-09 05:35:07 -0400

    made changes to formatting and corrections to constants (objects)

--- script.js
+++ script.js
@@ -1,84 +1,59 @@
-const start = document.getElementById("start");
+console.log(constants);
 
-const quiz = document.getElementById("quiz");
+const start = document.getEl("start");
+const quiz = document.getEl("quiz");
+const question = document.getEl("question");
+const choice1 = document.getEl("4");("1");
+const choice2 = document.getEl("4");("2");
+const choice3 = document.getEl("3");("3");
+const choice4 = document.getEl("3");("4");
+const choice5 = document.getEl("2");("5");
+const counter = document.getEl("counter");
+const timeGauge = document.getEl("timeGauge");
+const progress = document.getEl("progress");
+const scoreDiv = document.getEl("scoreContainer");
 
-const question = document.getElementById("question");
-
-const qImg = document.getElementById("qImg");
-
-const choiceA = document.getElementById("A");
-
-const choiceB = document.getElementById("B");
-
-const choiceC = document.getElementById("C");
-
-const counter = document.getElementById("counter");
-
-const timeGauge = document.getElementById("timeGauge");
-
-const progress = document.getElementById("progress");
-
-const scoreDiv = document.getElementById("scoreContainer");
+console.log(questions);
 
 let questions = [
 
     {
         question :"Arrays in JavaScript can be used to store _____.",
-
         choice1 : "Wrong",
-
         choice2 : "Wrong",
-
         choice3 : "Wrong",
-
         choice4 : "Correct",
 
    },{
 
         question : "A very useful tool used during development and debugging for printing content to the debugger is called ?",
-
         choice1 : "Wrong",
-
         choice2 : "Wrong",
-
         choice3 : "Wrong",
-
         choice4 : "Correct",
 
     },{
 
         question : "String values must be enclosed within _____ when being assigned to variables?",
-
         choice1 : "Wrong",
-
         choice2 : "Wrong",
-
         choice3 : "Correct",
-
         choice4 : "Wrong",
 
     },{
 
         question : "Commonly used data types DO NOT include:",
-
         choice1 : "Wrong",
-
         choice2 : "Wrong",
-
         choice3 : "Correct",
-
         choice4 : "Wrong",
 
     },{
 
         question : "The condition in an if/else statement is enclosed within_____:",
-
         choice1 : "Wrong",
-
         choice2 : "Correct",
-
         choice3 : "Wrong",
-
         choice4 : "Wrong",
 
     }
