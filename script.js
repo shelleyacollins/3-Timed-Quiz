13bff61ba0e241e06a341f4da4e0317fe1885a07
Shelley <sascottcollins@gmail.com>
2020-09-09 05:16:43 -0400

    added questions and answers to JS

--- script.js
+++ script.js
@@ -19,3 +19,68 @@ const timeGauge = document.getElementById("timeGauge");
 const progress = document.getElementById("progress");
 
 const scoreDiv = document.getElementById("scoreContainer");
+
+let questions = [
+
+    {
+        question :"Arrays in JavaScript can be used to store _____.",
+
+        choice1 : "Wrong",
+
+        choice2 : "Wrong",
+
+        choice3 : "Wrong",
+
+        choice4 : "Correct",
+
+   },{
+
+        question : "A very useful tool used during development and debugging for printing content to the debugger is called ?",
+
+        choice1 : "Wrong",
+
+        choice2 : "Wrong",
+
+        choice3 : "Wrong",
+
+        choice4 : "Correct",
+
+    },{
+
+        question : "String values must be enclosed within _____ when being assigned to variables?",
+
+        choice1 : "Wrong",
+
+        choice2 : "Wrong",
+
+        choice3 : "Correct",
+
+        choice4 : "Wrong",
+
+    },{
+
+        question : "Commonly used data types DO NOT include:",
+
+        choice1 : "Wrong",
+
+        choice2 : "Wrong",
+
+        choice3 : "Correct",
+
+        choice4 : "Wrong",
+
+    },{
+
+        question : "The condition in an if/else statement is enclosed within_____:",
+
+        choice1 : "Wrong",
+
+        choice2 : "Correct",
+
+        choice3 : "Wrong",
+
+        choice4 : "Wrong",
+
+    }
+
+];
