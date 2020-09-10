162fc4d464d6e5dd26f4ce4f324cec66cd7d75f8
Shelley <sascottcollins@gmail.com>
2020-09-09 20:11:26 -0400

    added different formatting

--- script.js
+++ script.js
@@ -1,3 +1,77 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    {
+        question :"Arrays in JavaScript can be used to store _____.",
+        choice1 : "<1.numbers and strings",
+        choice2 : "<2.other arrays",
+        choice3 : "<3.booleans",
+        choice4 : "<4.all of the above",
+        answer: 4
+    },
+    {
+        question :"A very useful tool used during development and debugging for printing content to the debugger is:",
+        choice1 : "<1.JavaScript",
+        choice2 : "<2.terminal/bash",
+        choice3 : "<3.for loops",
+        choice4 : "<4.console log",
+        answer: 4
+    },
+    {
+        question :"String values must be enclosed within_____ when being assigned to variables.",
+        choice1 : "<1.commas",
+        choice2 : "<2.curly brackets",
+        choice3 : "<3.quotes",
+        choice4 : "<4.parenthesis",
+        answer: 3
+    },
+    {
+        question :"Commonly used data types DO NOT include:",
+        choice1 : "<1.strings",
+        choice2 : "<2.booleans",
+        choice3 : "<3.alerts",
+        choice4 : "<4.numbers",
+        answer: 3
+    },
+    {
+        question :"The condition in an if/else statement is enclosed within _____.",
+        choice1 : "<1.quotes",
+        choice2 : "<2.curly brackets",
+        choice3 : "<3.parenthesis",
+        choice4 : "<4.square brackets",
+        answer: 2
+    },
+    {
+    
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 <div id="quiz"></div>
 <button id="submit">Submit Quiz</button>
 <div id="results"></div>
@@ -37,7 +111,7 @@ document.addEventListener('DOMContentLoaded', () {
     
 
 
-var questions = [
+const questions = [
     {
         question :"1. Arrays in JavaScript can be used to store _____.",
         choice1 : "Wrong",
