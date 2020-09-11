a1603517a273a69fbbc16574f6feaae4d0d57ea0
Shelley <sascottcollins@gmail.com>
2020-09-11 07:37:30 -0400

    wrapped everything inside a document element

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,10 @@
-console.log(questions);
+$(document).ready(function() {
+    console.log(questions);
 
-$("#start-quiz").on("click", function() {
-    // Hide the welcome screen
-    // Start the timer
-    //Show the first question.
-    alert("Start Quiz");
-})
+    $("#start-quiz").on("click", function() {
+        // Hide the welcome screen
+        // Start the timer
+        //Show the first question.
+        alert("Start the Quiz!");
+    });
+});
