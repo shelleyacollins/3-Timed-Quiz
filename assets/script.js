55dcdcb0af2cb1dda855bcb8a14d8313a0202354
Shelley <sascottcollins@gmail.com>
2020-09-11 07:27:07 -0400

    added an alert to test button

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
 console.log(questions);
 
-$("#start-quiz")
+$("#start-quiz").on("click", function() {
+    // Hide the welcome screen
+    // Start the timer
+    //Show the first question.
+    alert("Start Quiz");
+})
