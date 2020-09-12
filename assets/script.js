3e8023d706ae539abe632b4778a1e0b8aef31e5e
Shelley <sascottcollins@gmail.com>
2020-09-12 10:27:39 -0400

    added for loop and structure for button display on questions

--- assets/script.js
+++ assets/script.js
@@ -8,16 +8,25 @@ $(document).ready(function() {
         var questionDiv = $("#question");
 
         var h1El = $("<h1>");
-        h1El.text("Arrays in JavaScript can be used to store _____.")
-
-        for (var i = 0; i < 4; i++) {
-            var buttonEl = $("<button" > );
-            buttonEl.addClass("btn btn-secondary");
-            btn.text("2");
-        }
+        h1El.text(questions[0].title);
+        h1El.text("Arrays in JavaScript can be used to store _____.");
         questionDiv.append(h1El);
-        questionDiv.append(buttonEl);
+        var ulEl = $("<ul>");
 
-        alert("Start the Quiz!");
+        for (var i = 0; i < questions[0].choices; i++) {
+            console.log(questions[0].choices[i]);
+            var liEl = $("<li>");
+            var buttonEl = $("<button");
+            buttonEl.addClass("btn btn-secondary");
+            buttonEl.text("questions[0].choices[i]");
+            liEl.append(buttonEl);
+            ulEl.append(liEl);
+        }
+        questionDiv.append(ulEl);
+
+
+
+
+
     });
 });
