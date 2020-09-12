3d78591eefd7baff6c57ef3173040000afbd024d
Shelley <sascottcollins@gmail.com>
2020-09-12 11:47:11 -0400

    added event listener functions to start game and added timer parameters

--- assets/script.js
+++ assets/script.js
@@ -10,31 +10,46 @@ $(document).ready(function() {
                 var h1El = $("<h1>");
                 h1El.text(questions[currentQuestionIndex].title);
                 questionDiv.append(h1El);
-                h1El.text("Arrays in JavaScript can be used to store _____.");
-                questionDiv.append(h1El);
                 var ulEl = $("<ul>");
 
-                for (var i = 0; i < questions[0].choices.length; i++) {
+                for (var i = 0; i < questions[currentQuestionIndex].choices.length; i++) {
                     var liEl = $("<li>");
-                    var buttonEl = $("<button");
-                    buttonEl.addClass("btn btn-secondary");
+                    var buttonEl = $("<button>");
+                    buttonEl.addClass("btn btn-secondary answer");
                     buttonEl.text(questions[currentQuestionIndex].choices[i]);
+                    buttonEl.attr("data-choice", questions[currentQuestionIndex].choices[i]);
                     liEl.append(buttonEl);
                     ulEl.append(liEl);
                 }
+
                 questionDiv.append(ulEl);
-                // Hide the welcome screen
-                $("#start-quiz").on("click", function() {
 
-                    $("#welcome-screen").attr("style", "display: none");
-
-                    displayQuestion(); // Start the timer
-                    //Show the first question.
-
+                function setAndClearIndicator(text) {
+                    correctIndicatorEl.text(text);
+                    setTimeout(function() {
+                        correctIndicatorEl.text("");
+                    }, 1000);
 
 
+                    $("#start-game").on("click", function() {
+                        $("#welcome-screen").attr("style", "display: none");
+                        timeInterval = setInterval(function() {
+                            $("#time-remaining").text(timeRemaining);
+                            timeRemaining--;
+                        }, 1000);
+
+                        // Hide the welcome screen
+                        $("#start-quiz").on("click", function() {
+
+                            $("#welcome-screen").attr("style", "display: none");
+
+                            displayQuestion(); // Start the timer
+                            //Show the first question.
 
 
 
-                });
-            });
+
+
+
+                        });
+                    });
