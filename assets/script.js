358554e41b85186949852bc2e7c78fea96fdfcaf
Shelley <sascottcollins@gmail.com>
2020-09-12 09:37:13 -0400

    targeting secondary button block

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,18 @@ $(document).ready(function() {
 
     $("#start-quiz").on("click", function() {
         // Hide the welcome screen
-        $("#welcome-screen")
-            .attr("style", "display: none"); // Start the timer
+        $("#welcome-screen").attr("style", "display: none"); // Start the timer
         //Show the first question.
+        var questionDiv = $("#question");
+
+        var h1El = $("<h1>");
+        h1El.text("Arrays in JavaScript can be used to store _____.")
+
+        var buttonEl = $("<button" > );
+        buttonEl.addClass("btn btn-secondary");
+        btn.text("2");
+        questionDiv.append(h1El);
+
         alert("Start the Quiz!");
     });
 });
