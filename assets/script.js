8d5f0359bf9c879baa636f0a825e2733b11948c1
Shelley <sascottcollins@gmail.com>
2020-09-12 11:29:22 -0400

    added timer variables and cleaned up spacing

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,42 @@
-console.log(questions);
-
-var currentQuestion = 0;
-
-function displayQuestion() {
-
+$(document).ready(function() {
     var questionDiv = $("#question");
-
-}
-
-
-
-
-
-var h1El = $("<h1>");
-h1El.text(questions[0].title);
-h1El.text("Arrays in JavaScript can be used to store _____.");
-questionDiv.append(h1El);
-var ulEl = $("<ul>");
-
-for (var i = 0; i < questions[0].choices.length; i++) {
-    var liEl = $("<li>");
-    var buttonEl = $("<button");
-    buttonEl.addClass("btn btn-secondary");
-    buttonEl.text(questions[currentQuestionIndex].choices[i]);
-    liEl.append(buttonEl);
-    ulEl.append(liEl);
-}
-questionDiv.append(ulEl);
-// Hide the welcome screen
-$("#start-quiz").on("click", function() {
-
-$("#welcome-screen").attr("style", "display: none");
-
-displayQuestion(); // Start the timer
-//Show the first question.
+    var correctIndicatorEl = $("#correct-indicator");
+    var currentQuestionIndex = 0;
+    var timeRemaining = 90;
+    var timeInterval;
 
 
 
 
 
 
-});
+    var h1El = $("<h1>");
+    h1El.text(questions[0].title);
+    h1El.text("Arrays in JavaScript can be used to store _____.");
+    questionDiv.append(h1El);
+    var ulEl = $("<ul>");
+
+    for (var i = 0; i < questions[0].choices.length; i++) {
+        var liEl = $("<li>");
+        var buttonEl = $("<button");
+        buttonEl.addClass("btn btn-secondary");
+        buttonEl.text(questions[currentQuestionIndex].choices[i]);
+        liEl.append(buttonEl);
+        ulEl.append(liEl);
+    }
+    questionDiv.append(ulEl);
+    // Hide the welcome screen
+    $("#start-quiz").on("click", function() {
+
+        $("#welcome-screen").attr("style", "display: none");
+
+        displayQuestion(); // Start the timer
+        //Show the first question.
+
+
+
+
+
+
+    });
 });
